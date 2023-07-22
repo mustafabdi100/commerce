c98d531b83663add65d526a88abb3c1f7aff5d7a
mustafabdi100 <mustafa300abdi@gmail.com>
2023-07-22 23:02:19 +0300

    initial commit

---
+++ ecommerce/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('', include('authentication.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
